43c0fda1ce21cf400735a6639adfeb1a285c02da
kexun <abcpeter95@163.com>
2025-08-07 09:53:45 +0100

    2nd commit - java repo final这一节学完了

--- oop-adv/src/d3_final/Test.java
+++ oop-adv/src/d3_final/Test.java
@@ -1,6 +1,9 @@
 package d3_final;
 
 public class Test {
+    public static final String SCHOOL_NAME = "TCD";  // 如果final对静态变量进行修饰，那么一定要同时完成赋值
+    // 用public static final修饰的成员变量我们称之为常量， 建议名称全部大写，多个单词下划线连接
+
     public static void main(String[] args) {
         // 目标： 认识final的作用
         // 3. final可以修饰变量总规则： 有且仅能赋值一次
@@ -11,10 +14,19 @@ public class Test {
                 2、实例成员变量
          */
         final int a;  // a就是一个局部变量，因为它被定义在 main 方法内部，因此它的作用域仅限于 main 方法内
+        a = 12;
+        // a = 13;    // 第二次赋值，出错了
 
+        final double r = 3.14;
+        // r = 0.01;   // 道理同上
 
+        final int[] arr_1 = {11,22,33};
+        arr_1 [0] = 33;
+        // final 修饰基本类型的变量，变量存储的数据不能被改变
+        // final 修饰引用类型的变量，变量存储的地址不能改变，但地址所指向对象的内容是可以被改变的
+    }
 
-
-
+    public static void buy(final double z){
+        // z = 0.1;     // 这里的逻辑是，其他方法在调用buy()的时候，会向其中传入z的值，而z的值无法在方法buy()内部被修改，因为已是final
     }
 }
